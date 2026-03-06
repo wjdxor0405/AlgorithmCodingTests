e3c0d6b8d6f7a0b59927da74bbe06da44e57ccee
limjt0 <100699283+wjdxor0405@users.noreply.github.com>
2026-03-06 17:46:02 +0900

    [level 1] Title: 삼총사, Time: 0.01 ms, Memory: 4.23 MB -BaekjoonHub

---
+++ 프로그래머스/1/131705. 삼총사/삼총사.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+#include <stdbool.h>
+#include <stdlib.h>
+
+int solution(int number[], size_t number_len) {
+    int answer = 0;
+    for(int i=0;i<number_len;i++){
+        for(int j=i+1;j<number_len;j++){
+            for(int k=j+1;k<number_len;k++){
+                if(number[i]+number[j]+number[k] == 0)answer++;
+            }
+        }
+    }
+    return answer;
+}
