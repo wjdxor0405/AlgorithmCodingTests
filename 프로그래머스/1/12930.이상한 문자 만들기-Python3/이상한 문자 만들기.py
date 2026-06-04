f17ca48c07fe674110059fb60a75b8332fd4f3e5
limjt0 <100699283+wjdxor0405@users.noreply.github.com>
2026-06-05 08:38:33 +0900

    [level 1] Title: 이상한 문자 만들기, Time: 0.02 ms, Memory: 11.2 MB -BaekjoonHub

---
+++ 프로그래머스/1/12930.이상한 문자 만들기-Python3/이상한 문자 만들기.py
@@ -0,0 +1,13 @@
+def solution(s):
+    answer = ''
+    words = s.split(' ')
+    for w in words:
+        for i in range(len(w)):
+            if i % 2 == 0:
+                answer += w[i].upper()
+            else:
+                answer += w[i].lower()
+        answer += ' '
+            
+            
+    return answer[:-1]
